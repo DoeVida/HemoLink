76b893f92367a7c51672b235197b460657cfd250
GABRIEL NASARIO ANTUNES <gabriel.antunes4@alunos.sc.senac.br>
2024-08-30 16:08:00 -0300

    Criada a classe usuario

---
+++ src/main/java/br/senac/hemolink/modelo/entidade/Usuario.java
@@ -0,0 +1,41 @@
+package br.senac.hemolink.modelo.entidade;
+
+public class Usuario {
+	
+	private String nome;
+	private String senha;
+	private Papel papel;
+	private Contato contato;
+	
+	public void setNome (String nome) {
+		this.nome = nome;
+	}
+	
+	public String getNome () {
+		return this.nome;
+	}
+	
+	public void setSenha (String senha) {
+		this.senha = senha;
+	}
+
+	public String getSenha () {
+		return this.senha;
+	}
+	
+	public void setPapel (String papel) {
+		this.papel = new Papel(papel);
+	}
+	
+	public Papel getPapel () {
+		return this.papel;
+	}
+	
+	public void setContato (String contato) {
+		this.contato = new Contato(contato);
+	}
+	
+	public Contato () {
+		return this.contato;
+	}
+}
