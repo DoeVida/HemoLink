17b3448a56c783ef1c34ea5c3a0df0682754b541
Mariana-Borges <mariana.cb@aluno.ifsc.edu.br>
2024-08-30 15:33:35 -0300

    Criada a classe AquisicaoConquista

---
+++ src/main/java/br/senac/hemolink/modelo/entidade/AquisicaoConquista.java
@@ -0,0 +1,45 @@
+package br.senac.hemolink.modelo.entidade;
+
+import java.time.LocalDate;
+
+public class AquisicaoConquista {
+	private LocalDate dataAquisicao;
+	private Conquista conquista;
+	private Doador doador;
+	
+	public AquisicaoConquista(){}
+
+	public AquisicaoConquista(LocalDate dataAquisicao, Conquista conquista, Doador doador){
+		this.dataAquisicao = dataAquisicao;
+        this.conquista = conquista;
+        this.doador = doador;
+	}
+
+	public LocalDate getDataAquisicao() {
+		return dataAquisicao;
+	}
+
+	public void setDataAquisicao(LocalDate dataAquisicao) {
+		this.dataAquisicao = dataAquisicao;
+	}
+
+	public Conquista getConquista() {
+		return conquista;
+	}
+
+	public void setConquista(Conquista conquista) {
+		this.conquista = conquista;
+	}
+
+	public Doador getDoador() {
+		return doador;
+	}
+
+	public void setDoador(Doador doador) {
+		this.doador = doador;
+	}
+
+	
+	
+
+}
