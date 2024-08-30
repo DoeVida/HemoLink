1e0c414dfbd05e94ad8796f045cf8f7e08cd788a
GABRIEL NASARIO ANTUNES <gabriel.antunes4@alunos.sc.senac.br>
2024-08-30 14:12:12 -0300

    Adicionado classe Papel

--- src/main/java/br/senac/hemolink/modelo/entidade/papel/Papel.java
+++ src/main/java/br/senac/hemolink/modelo/entidade/papel/Papel.java
@@ -2,4 +2,22 @@ package br.senac.hemolink.modelo.entidade.papel;
 
 public class Papel {
 
+	private long idPapel;
+	private String nomePapel;
+	
+	public void setIdPapel (long idPapel) {
+		this.idPapel = idPapel;
+	}
+	
+	public long getIdPapel () {
+		return this.idPapel;
+	}
+	
+	public void setNomePapel (String nomePapel) {
+		this.nomePapel = nomePapel;
+	}
+	
+	public String getNomePapel () {
+		return this.nomePapel;
+	}
 }
