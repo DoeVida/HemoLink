8231f6d7356a3132df4891d49d3773257339a79f
NIKOLAS ALEXI <nikolas.alexi@alunos.sc.senac.br>
2024-09-06 16:19:17 -0300

    Criado o pacote Campanha'

---
+++ src/main/java/br/senac/hemolink/modelo/entidade/campanha/Campanha.java
@@ -0,0 +1,34 @@
+package br.senac.hemolink.modelo.entidade.campanha;
+
+import java.util.List;
+
+class Campanha {
+    private String titulo;
+    private String descricao;
+    private List<Demanda> demanda;
+
+    // Get Set
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public List<Demanda> getDemanda() {
+        return demanda;
+    }
+
+    public void setDemanda(List<Demanda> demanda) {
+        this.demanda = demanda;
+    }
+}
