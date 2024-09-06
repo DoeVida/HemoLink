536501920638aa330e72e85eea9ce3a578cae536
BARBARA GEOVANA BORGES VAZ <barbara.vaz@alunos.sc.senac.br>
2024-09-06 16:43:17 -0300

    criada classe Conquista

--- src/main/java/br/senac/hemolink/modelo/entidade/conquista/Conquista.java
+++ src/main/java/br/senac/hemolink/modelo/entidade/conquista/Conquista.java
@@ -2,4 +2,39 @@ package br.senac.hemolink.modelo.entidade.conquista;
 
 public class Conquista {
 
+	private int idConquista;
+	private String nomeConquista;
+	private String descricaoConquista;
+	
+	public Conquista () {}
+	
+	public Conquista (int idConquista, String nomeConquista, String descricaoConquista) {
+		this.idConquista = idConquista;
+		this.nomeConquista = nomeConquista;
+		this.descricaoConquista = descricaoConquista;
+		
+	}
+	
+	public void setIdConquista (int idConquista) {
+		this.idConquista = idConquista;
+	}
+	
+	public int getIdConquista() {
+		return idConquista;
+	}
+	
+	public void setNomeConquista (String nomeConquista) {
+		this.nomeConquista = nomeConquista;
+	}
+	public String getNomeConquista () {
+		return nomeConquista;
+	}
+	
+	public void setDescricaoConquista (String descricaoConquista) {
+		this.descricaoConquista = descricaoConquista;
+	}
+	
+	public String getDescxricaoConquista (){
+		return descricaoConquista;
+	}
 }
