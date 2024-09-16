a489ca98042d475258ecf19155f6dceea6fe6316
HenriqueEberhardt <159157371+HenriqueEberhardt@users.noreply.github.com>
2024-09-16 14:42:12 -0300

    Implementada a classe EstoqueSangue e corrigido imports

--- src/main/java/br/senac/hemolink/modelo/entidade/estoqueSangue/EstoqueSangue.java
+++ src/main/java/br/senac/hemolink/modelo/entidade/estoqueSangue/EstoqueSangue.java
@@ -1,9 +1,27 @@
 package br.senac.hemolink.modelo.entidade.estoqueSangue;
 
+import java.io.Serializable;
+import java.util.Objects;
+import java.time.LocalDate;
+import javax.persistence.*;
 import br.senac.hemolink.modelo.enumeracao.TipoSanguineo;
 
-public class EstoqueSangue {
+@Entity
+@Table (name = "estoque")
+
+public class EstoqueSangue implements Serializable {
+    
+    private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id_estoque", nullable = false, unique = true)
+	private int idEstoqueSangue;
+	
+	@Column(name = "tipo_sanguineo", length = 3, nullable = false, unique = false)
 	private TipoSanguineo tipoSanguineo;
+	
+	@Column(name = "quantidade_disponivel", nullable = false, unique = false)
 	private float quantidadeDisponivel;
 
 	public EstoqueSangue() {
