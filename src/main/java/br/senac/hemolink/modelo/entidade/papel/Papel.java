75c3233de8603ce72b13fbf883c9dd99a2d0a876
EduardoHHemer <eduardohhemer@gmail.com>
2024-09-16 15:38:34 -0300

    Mapeada a classe Papel

--- src/main/java/br/senac/hemolink/modelo/entidade/papel/Papel.java
+++ src/main/java/br/senac/hemolink/modelo/entidade/papel/Papel.java
@@ -2,12 +2,24 @@ package br.senac.hemolink.modelo.entidade.papel;
 
 import java.io.Serializable;
 
+import javax.persistence.FetchType;
+import javax.persistence.JoinColumn;
+
 @Entity
 @Table(name= "papel")
 public class Papel implements Serializable{
 
 	@Id
-	@Column(name="idPapel") private Long id;
+	@Column(name="idPapel") 
+	private Long id;
+	
+	@Column(name="nomePapel", length = 50, nullable = false, unique = true)
+	private String nomePapel;
+	
+	@OnetoOne(fetch = FetchType.LAZY)
+    @MapsId
+    @JoinColumn(name = "idPapel")
+	
 	private int idPapel;
 	private String nomePapel;
 	
