9e258995eaed37ddd5522a6796b56ecb8104e724
NIKOLAS ALEXI <nikolas.alexi@alunos.sc.senac.br>
2024-09-16 15:18:17 -0300

    criado a classe doacao mapeada para banco de dados

--- src/main/java/br/senac/hemolink/modelo/entidade/doacao/Doacao.java
+++ src/main/java/br/senac/hemolink/modelo/entidade/doacao/Doacao.java
@@ -2,54 +2,81 @@ package br.senac.hemolink.modelo.entidade.doacao;
 
 import java.time.LocalDate;
 import java.time.LocalTime;
+import javax.persistence.*;
 import br.senac.hemolink.modelo.entidade.hemocentro.Hemocentro;
 import br.senac.hemolink.modelo.entidade.doador.Doador;
 
+@Entity
+@Table(name = "doacao")
 public class Doacao {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@ManyToOne
+	@JoinColumn(name = "doador_id", nullable = false)
 	private Doador doador;
+
+	@ManyToOne
+	@JoinColumn(name = "hemocentro_id", nullable = false)
 	private Hemocentro hemocentro;
+
+	@Column(name = "data_doacao", nullable = false)
 	private LocalDate dataDoacao;
+
+	@Column(name = "hora_doacao", nullable = false)
 	private LocalTime horaDoacao;
-	
-	public Doacao() {}
-	
-	public Doacao (Doador doador, Hemocentro hemocentro,LocalDate dataDoacao, LocalTime horaDoacao) {
+
+	public Doacao() {
+	}
+
+	public Doacao(Doador doador, Hemocentro hemocentro, LocalDate dataDoacao, LocalTime horaDoacao) {
 		this.doador = doador;
 		this.hemocentro = hemocentro;
 		this.dataDoacao = dataDoacao;
 		this.horaDoacao = horaDoacao;
 	}
-	
-	public void setDoador (Doador doador) {
-		this.doador = doador;
+
+	// Getters e Setters
+
+	public Long getId() {
+		return id;
 	}
-	
-	public Doador getDoador () {
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public Doador getDoador() {
 		return doador;
 	}
-	
-	public void setHemocentro (Hemocentro hemocentro) {
-		this.hemocentro = hemocentro;
+
+	public void setDoador(Doador doador) {
+		this.doador = doador;
 	}
-	
+
 	public Hemocentro getHemocentro() {
 		return hemocentro;
 	}
 
-	public void setData (LocalDate dataDoacao) {
-		this.dataDoacao = dataDoacao;
+	public void setHemocentro(Hemocentro hemocentro) {
+		this.hemocentro = hemocentro;
 	}
-	
-	public LocalDate getData () {
+
+	public LocalDate getDataDoacao() {
 		return dataDoacao;
 	}
-	
-	public void sethoraDoacao (LocalTime horaDoacao) {
-		this.horaDoacao = horaDoacao;
+
+	public void setDataDoacao(LocalDate dataDoacao) {
+		this.dataDoacao = dataDoacao;
 	}
-	
-	public LocalTime gethoraDoacao() {
+
+	public LocalTime getHoraDoacao() {
 		return horaDoacao;
+	}
+
+	public void setHoraDoacao(LocalTime horaDoacao) {
+		this.horaDoacao = horaDoacao;
 	}
 }
