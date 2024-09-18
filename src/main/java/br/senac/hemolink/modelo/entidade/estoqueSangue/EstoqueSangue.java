6ec4ea1897cd282d45e67f0d80d6e74ef8ca20ac
HENRIQUE EBERHARDT <henrique.eberhardt@alunos.sc.senac.br>
2024-09-18 13:41:48 -0300

    Atualizados Getters and Setters e construtores

--- src/main/java/br/senac/hemolink/modelo/entidade/estoqueSangue/EstoqueSangue.java
+++ src/main/java/br/senac/hemolink/modelo/entidade/estoqueSangue/EstoqueSangue.java
@@ -1,18 +1,46 @@
 package br.senac.hemolink.modelo.entidade.estoqueSangue;
 
+import java.io.Serializable;
+import java.util.Objects;
+import java.time.LocalDate;
+import javax.persistence.*;
 import br.senac.hemolink.modelo.enumeracao.TipoSanguineo;
 
-public class EstoqueSangue {
+@Entity
+@Table (name = "estoque")
+
+public class EstoqueSangue implements Serializable {
+    
+    private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id_estoque", nullable = false, unique = true)
+	private int idEstoqueSangue;
+	
+	@Column(name = "tipo_sanguineo", length = 3, nullable = false, unique = false)
 	private TipoSanguineo tipoSanguineo;
+	
+	@Column(name = "quantidade_disponivel", nullable = false, unique = false)
 	private float quantidadeDisponivel;
 
-	public EstoqueSangue() {
+	public EstoqueSangue() {}
 
-	}
 
-	public EstoqueSangue(TipoSanguineo tipoSanguineo, float quantidadeDisponivel) {
+
+	public EstoqueSangue(int idEstoqueSangue, br.senac.hemolink.modelo.entidade.estoqueSangue.TipoSanguineo tipoSanguineo, float quantidadeDisponivel) {
+		super();
+		this.idEstoqueSangue = idEstoqueSangue;
 		this.tipoSanguineo = tipoSanguineo;
 		this.quantidadeDisponivel = quantidadeDisponivel;
+	}
+
+	public int getIdEstoqueSangue() {
+		return idEstoqueSangue;
+	}
+
+	public void setIdEstoqueSangue(int idEstoqueSangue) {
+		this.idEstoqueSangue = idEstoqueSangue;
 	}
 
 	public TipoSanguineo getTipoSanguineo() {
@@ -30,4 +58,8 @@ public class EstoqueSangue {
 	public void setQuantidadeDisponivel(float quantidadeDisponivel) {
 		this.quantidadeDisponivel = quantidadeDisponivel;
 	}
+	
+	
+
+	
 }
