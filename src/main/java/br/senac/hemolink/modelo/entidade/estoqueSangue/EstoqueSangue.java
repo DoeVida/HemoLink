d2caa59794fd364159552db902ba4bf84173442c
HENRIQUE EBERHARDT <henrique.eberhardt@alunos.sc.senac.br>
2024-09-18 15:38:52 -0300

    Atualizada classe EstoqueSangue com formatação

--- src/main/java/br/senac/hemolink/modelo/entidade/estoqueSangue/EstoqueSangue.java
+++ src/main/java/br/senac/hemolink/modelo/entidade/estoqueSangue/EstoqueSangue.java
@@ -7,26 +7,25 @@ import java.time.LocalDate;
 import br.senac.hemolink.modelo.enumeracao.TipoSanguineo;
 
 @Entity
-@Table (name = "estoque")
+@Table(name = "estoque")
 public class EstoqueSangue implements Serializable {
-    private static final long serialVersionUID = 1L;
-	
+	private static final long serialVersionUID = 1L;
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	@Column(name = "id_estoque", nullable = false, unique = true)
 	private int idEstoqueSangue;
-	
+
 	@Column(name = "tipo_sanguineo", length = 3, nullable = false, unique = false)
 	private TipoSanguineo tipoSanguineo;
-	
+
 	@Column(name = "quantidade_disponivel", nullable = false, unique = false)
 	private float quantidadeDisponivel;
 
 	public EstoqueSangue() { }
 
-
-
-	public EstoqueSangue(int idEstoqueSangue, br.senac.hemolink.modelo.entidade.estoqueSangue.TipoSanguineo tipoSanguineo, float quantidadeDisponivel) {
+	public EstoqueSangue(int idEstoqueSangue,
+			br.senac.hemolink.modelo.entidade.estoqueSangue.TipoSanguineo tipoSanguineo, float quantidadeDisponivel) {
 		super();
 		this.idEstoqueSangue = idEstoqueSangue;
 		this.tipoSanguineo = tipoSanguineo;
