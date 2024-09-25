3e4f52c94b7140d8b81833bf261ae5524d1169dd
GABRIEL NASARIO ANTUNES <gabriel.antunes4@alunos.sc.senac.br>
2024-09-25 16:44:39 -0300

    Criado o DAO da classe Campanha

---
+++ src/main/java/br/senac/hemolink/modelo/dao/campanha/CampanhaDAO.java
@@ -0,0 +1,12 @@
+package br.senac.hemolink.modelo.dao.campanha;
+
+import br.senac.hemolink.modelo.entidade.campanha.Campanha;
+
+public interface CampanhaDAO {
+
+	void inserirCampanha (Campanha campanha);
+	
+	void deletarCampanha (Campanha campanha);
+	
+	void atualizarCampanha (Campanha campanha);
+}
