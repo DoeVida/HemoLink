a2ad08ab0b2a5f30ea6ffbccd93934c12fed957e
GABRIEL NASARIO ANTUNES <gabriel.antunes4@alunos.sc.senac.br>
2024-09-27 13:48:17 -0300

    Corrigido o DAO e DAOImpl da classe Papel

--- src/main/java/br/senac/hemolink/modelo/dao/papel/PapelDAO.java
+++ src/main/java/br/senac/hemolink/modelo/dao/papel/PapelDAO.java
@@ -1,5 +1,12 @@
 package br.senac.hemolink.modelo.dao.papel;
 
-public class PapelDAO {
+import br.senac.hemolink.modelo.entidade.papel.Papel;
 
+public interface PapelDAO {
+	
+	void adicionarPapel (Papel papel);
+	
+	void deletarPapel (Papel papel);
+	
+	void atualizarPapel (Papel papel);
 }
