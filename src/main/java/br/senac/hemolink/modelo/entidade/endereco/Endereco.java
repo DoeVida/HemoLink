bb9cb93fe13364c32fc54a03b32b7274b6d7b9e3
HENRIQUE EBERHARDT <henrique.eberhardt@alunos.sc.senac.br>
2024-09-27 16:47:47 -0300

    Atualizada classe teste

--- src/main/java/br/senac/hemolink/modelo/entidade/endereco/Endereco.java
+++ src/main/java/br/senac/hemolink/modelo/entidade/endereco/Endereco.java
@@ -1,17 +1,50 @@
 package br.senac.hemolink.modelo.entidade.endereco;
 
-public class Endereco {
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name= "Endereco")
+public class Endereco implements Serializable {
 	
-	private String estado;
-	private String cidade;
-	private String bairro;
-	private String logradouro;
-	private int numero;
-	private String cep;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_endereco", nullable = false, unique = true )
+    private int idEndereco; 
+
+    @Column(name = "estado", nullable = false, length = 2)
+    private String estado;
+
+    @Column(name = "cidade_endereco", nullable = false, length = 45)
+    private String cidade;
+
+    @Column(name = "bairro_endereco", nullable = false, length = 45)
+    private String bairro;
+
+    @Column(name = "logradouro_endereco", nullable = false, length = 45)
+    private String logradouro;
+
+    @Column(name = "numero_endereco", nullable = false)
+    private int numero;
+
+    @Column(name = "cep_endereco", nullable = false, length = 8)
+    private String cep;
+
+    @OneToOne
+    @JoinColumn (name = "nickname_hemocentro", nullable = false)
+    private Hemocentro hemocentro;
 	
-	public void Endereco() {}
+	public Endereco() {}
 	
-	public void Endereco (String estado, String cidade, String bairro, String logradouro, int numero, String cep) {
+	public Endereco (String estado, String cidade, String bairro, String logradouro, int numero, String cep) {
 		this.estado = estado;
 		this.cidade = cidade;
 		this.bairro = bairro;
--- src/main/java/teste/Main.java
+++ src/main/java/teste/Main.java
@@ -1,32 +1,33 @@
 package teste;
 
 import br.senac.hemolink.modelo.dao.contato.ContatoDAOImpl;
+import br.senac.hemolink.modelo.dao.endereco.EnderecoDAOImpl;
 import br.senac.hemolink.modelo.entidade.contato.Contato;
+import br.senac.hemolink.modelo.entidade.endereco.Endereco;
 
 public class Main {
 
 	public static void main(String[] args) {
-		ContatoDAOImpl ContatoDAO = new ContatoDAOImpl();
-		Contato c1 = new Contato();
-		c1.setEmail("ssss");
-		c1.setIdContato(1);
-		c1.setTelefone("9999");
-		
-		Contato c2 = new Contato();
-		c2.setEmail("aaaa");
-		c2.setIdContato(23);
-		c2.setTelefone("8888");
-		
-				
-		try {
-			ContatoDAO.inserirContato(c1);
-		} catch (Exception e) {
-			System.out.println("Erro no inserir");
-		}finally {
-			System.out.println("Acabou!!!");
-		}
 		
 		
+		//Endereço
+		EnderecoDAOImpl EnderecoDAO = new EnderecoDAOImpl();
+		Endereco e1 = new Endereco();
+		e1.setBairro("Centro");
+		e1.setCep("89-000000");
+		e1.setCidade("Blumenau");
+		e1.setEstado("Santa Catarina");
+		e1.setLogradouro("Hemocentro");
+		e1.setNumero(34);
 		
+		// Teste de insert de dados
+				try {
+					EnderecoDAO.inserirEndereco(e1);
+				} catch (Exception e) {
+					System.out.println("Erro no inserir");
+				} finally {
+					System.out.println("Acabou!!!");
+				}
+
 	}
 }
