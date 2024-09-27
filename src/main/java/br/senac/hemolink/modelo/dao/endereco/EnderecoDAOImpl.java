4fab0e16c021d81d421f95a706efa7ea68a13d72
GABRIEL NASARIO ANTUNES <gabriel.antunes4@alunos.sc.senac.br>
2024-09-27 16:04:52 -0300

    Corrigido o DAOImpl da classe Endereco

--- src/main/java/br/senac/hemolink/modelo/dao/endereco/EnderecoDAOImpl.java
+++ src/main/java/br/senac/hemolink/modelo/dao/endereco/EnderecoDAOImpl.java
@@ -1,5 +1,103 @@
 package br.senac.hemolink.modelo.dao.endereco;
 
-public class EnderecoDAOImpl {
+import org.hibernate.Session;
+
+import br.senac.hemolink.modelo.entidade.endereco.Endereco;
+import br.senac.hemolink.modelo.factory.conexao.ConexaoFactory;
+
+public class EnderecoDAOImpl implements EnderecoDAO{
+	
+	private ConexaoFactory fabrica;
+	
+	public EnderecoDAOImpl () {
+		fabrica = new ConexaoFactory();
+	}
+
+	public void inserirEnderecos(Endereco endereco) {
+		
+		Session sessao = null;
+
+		try {
+
+			sessao = fabrica.getConexao().openSession();
+			sessao.beginTransaction();
+
+			sessao.save(endereco);
+
+			sessao.getTransaction().commit();
+
+		} catch (Exception sqlException) {
+
+			sqlException.printStackTrace();
+
+			if (sessao.getTransaction() != null) {
+				sessao.getTransaction().rollback();
+			}
+
+		} finally {
+
+			if (sessao != null) {
+				sessao.close();
+			}
+		}
+	}
+
+	public void deletarEnderecos(Endereco endereco) {
+
+		Session sessao = null;
+
+		try {
+
+			sessao = fabrica.getConexao().openSession();
+			sessao.beginTransaction();
+
+			sessao.delete(endereco);
+
+			sessao.getTransaction().commit();
+
+		} catch (Exception sqlException) {
+
+			sqlException.printStackTrace();
+
+			if (sessao.getTransaction() != null) {
+				sessao.getTransaction().rollback();
+			}
+
+		} finally {
+
+			if (sessao != null) {
+				sessao.close();
+			}
+		}
+	}
+	
+	public void atualizarEndereco(Endereco endereco) {
+
+		Session sessao = null;
+
+		try {
+
+			sessao = fabrica.getConexao().openSession();
+			sessao.beginTransaction();
+
+			sessao.update(endereco);
+
+			sessao.getTransaction().commit();
+
+		} catch (Exception sqlException) {
+
+			sqlException.printStackTrace();
+
+			if (sessao.getTransaction() != null) {
+				sessao.getTransaction().rollback();
+			}
+
+		} finally {
+
+			if (sessao != null) {
+				sessao.close();
+			}
+		}
+	}
 
 }
