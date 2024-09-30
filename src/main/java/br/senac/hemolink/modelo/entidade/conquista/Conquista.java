173861596fe97d2d4f95263af25ede1c5bcae7e7
HENRIQUE EBERHARDT <henrique.eberhardt@alunos.sc.senac.br>
2024-09-30 14:05:53 -0300

    Atualizada classe conquista com espaçamentos corretos

--- src/main/java/br/senac/hemolink/modelo/entidade/conquista/Conquista.java
+++ src/main/java/br/senac/hemolink/modelo/entidade/conquista/Conquista.java
@@ -25,9 +25,7 @@ public class Conquista implements Serializable {
 	@Column(name = "descricao_conquista", length = 100, nullable = false, unique = true)
 	private String descricaoConquista;
 
-	public Conquista () {
-
-	}
+	public Conquista () { }
 
 	public Conquista (int idConquista, String nomeConquista, String descricaoConquista) {
 		this.idConquista = idConquista;
