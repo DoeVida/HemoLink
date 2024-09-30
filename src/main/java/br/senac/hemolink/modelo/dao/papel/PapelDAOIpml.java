b7a1c7d49db617f8b1aaf6f7261bd7ea32276e35
GABRIEL NASARIO ANTUNES <gabriel.antunes4@alunos.sc.senac.br>
2024-09-30 14:16:49 -0300

    Criado o DAOImpl da classe Papel

---
+++ src/main/java/br/senac/hemolink/modelo/dao/papel/PapelDAOIpml.java
@@ -0,0 +1,103 @@
+package br.senac.hemolink.modelo.dao.papel;
+
+import org.hibernate.Session;
+
+import br.senac.hemolink.modelo.entidade.papel.Papel;
+import br.senac.hemolink.modelo.factory.conexao.ConexaoFactory;
+
+public class PapelDAOIpml implements PapelDAO {
+
+	private ConexaoFactory fabrica;
+
+	public PapelDAOIpml() {
+		fabrica = new ConexaoFactory();
+	}
+
+	public void inserirPapel(Papel papel) {
+
+		Session sessao = null;
+
+		try {
+
+			sessao = fabrica.getConexao().openSession();
+			sessao.beginTransaction();
+
+			sessao.save(papel);
+
+			sessao.getTransaction().commit();
+
+		} catch (Exception sqlException) {
+
+			sqlException.printStackTrace();
+
+			if (sessao.getTransaction() != null) {
+				sessao.getTransaction().rollback();
+			}
+
+		} finally {
+
+			if (sessao != null) {
+				sessao.close();
+			}
+		}
+	}
+
+	public void deletarPapel(Papel papel) {
+
+		Session sessao = null;
+
+		try {
+
+			sessao = fabrica.getConexao().openSession();
+			sessao.beginTransaction();
+
+			sessao.delete(papel);
+
+			sessao.getTransaction().commit();
+
+		} catch (Exception sqlException) {
+
+			sqlException.printStackTrace();
+
+			if (sessao.getTransaction() != null) {
+				sessao.getTransaction().rollback();
+			}
+
+		} finally {
+
+			if (sessao != null) {
+				sessao.close();
+			}
+		}
+	}
+
+	@Override
+	public void atualizarPapel(Papel papel) {
+
+		Session sessao = null;
+
+		try {
+
+			sessao = fabrica.getConexao().openSession();
+			sessao.beginTransaction();
+
+			sessao.update(papel);
+
+			sessao.getTransaction().commit();
+
+		} catch (Exception sqlException) {
+
+			sqlException.printStackTrace();
+
+			if (sessao.getTransaction() != null) {
+				sessao.getTransaction().rollback();
+			}
+
+		} finally {
+
+			if (sessao != null) {
+				sessao.close();
+			}
+		}
+	}
+}
