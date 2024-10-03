569b5954aaf84285aea6a558b8bf71a212fe3f95
Barbaravazz <barbaraborgesvaz@gmail.com>
2024-10-03 16:38:15 -0300

    Corrigido o mapeamento da classe EstoqueSangue

--- src/main/java/br/senac/hemolink/modelo/entidade/estoqueSangue/EstoqueSangue.java
+++ src/main/java/br/senac/hemolink/modelo/entidade/estoqueSangue/EstoqueSangue.java
@@ -4,56 +4,42 @@ import java.io.Serializable;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.ManyToOne;
-import javax.persistence.MapsId;
 import javax.persistence.Table;
 
 import br.senac.hemolink.modelo.enumeracao.TipoSanguineo;
 
 	@Entity
-	@Table(name = "EstoqueDeSangue")
+	@Table(name = "EstoqueSangue")
 	public class EstoqueSangue implements Serializable {
+		
 	    private static final long serialVersionUID = 1L;
 
 		@Id
 		@GeneratedValue(strategy = GenerationType.IDENTITY)
-		@Column(name = "id_estoqueDeSangue", nullable = false, unique = true)
+		@Column(name = "id_estoque_sangue", nullable = false, unique = true)
 		private int idEstoqueSangue;
 
-		@ManyToOne(fetch = FetchType.LAZY)
-		@MapsId
-		@Column(name = "tipoSanguineo_estoqueDeSangue", nullable = false)
+		@ManyToOne
+		@Column(name = "tipo_sanguineo_estoque_sangue", nullable = false)
 		private TipoSanguineo tipoSanguineo;
 		
-		@Column(name = "quantidadeDisponivel_estoqueDeSangue", nullable = false)
+		@Column(name = "quantidade_disponivel_estoque_sangue", nullable = false)
 		private float quantidadeDisponivel;
 		
 		@Column(name = "id_armazenamento")
 		private int idArmazenamento;
 
+		public EstoqueSangue() {}
 		
-		public EstoqueSangue(int idEstoqueSangue, TipoSanguineo tipoSanguineo, float quantidadeDisponivel,
-				int idArmazenamento) {
+		public EstoqueSangue(int idEstoqueSangue, TipoSanguineo tipoSanguineo, float quantidadeDisponivel,int idArmazenamento) {
 			this.idEstoqueSangue = idEstoqueSangue;
 			this.tipoSanguineo = tipoSanguineo;
 			this.quantidadeDisponivel = quantidadeDisponivel;
 			this.idArmazenamento = idArmazenamento;
-		}
-
-		public EstoqueSangue() {
-			
-		}
-
-		public int getIdEstoqueSangue() {
-			return idEstoqueSangue;
-		}
-
-		public void setIdEstoqueSangue(int idEstoqueSangue) {
-			this.idEstoqueSangue = idEstoqueSangue;
 		}
 
 		public TipoSanguineo getTipoSanguineo() {
@@ -79,6 +65,4 @@ import br.senac.hemolink.modelo.enumeracao.TipoSanguineo;
 		public void setIdArmazenamento(int idArmazenamento) {
 			this.idArmazenamento = idArmazenamento;
 		}
-
-		
 }
