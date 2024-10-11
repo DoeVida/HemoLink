fd96cb21bca5e3892eaace36524f12ddc61028b9
marianaacb <mariana.cb@aluno.ifsc.edu.br>
2024-10-11 16:49:05 -0300

    Corrigida herança hibernate

--- src/main/java/br/senac/hemolink/modelo/entidade/usuario/hemocentro/Hemocentro.java
+++ src/main/java/br/senac/hemolink/modelo/entidade/usuario/hemocentro/Hemocentro.java
@@ -17,9 +17,12 @@ import javax.persistence.Table;
 
 import br.senac.hemolink.modelo.entidade.armazenamento.Armazenamento;
 import br.senac.hemolink.modelo.entidade.campanha.Campanha;
+import br.senac.hemolink.modelo.entidade.contato.Contato;
 import br.senac.hemolink.modelo.entidade.demanda.Demanda;
 import br.senac.hemolink.modelo.entidade.doacao.Doacao;
 import br.senac.hemolink.modelo.entidade.endereco.Endereco;
+import br.senac.hemolink.modelo.entidade.foto.Foto;
+import br.senac.hemolink.modelo.entidade.papel.Papel;
 import br.senac.hemolink.modelo.entidade.usuario.Usuario;
 
 @Entity
@@ -63,9 +66,9 @@ public class Hemocentro extends Usuario implements Serializable {
 	public Hemocentro() {
 	}
 
-	public Hemocentro(Usuario usuario, String cnpj, List<Demanda> demanda, Armazenamento armazenamento,
+	public Hemocentro(String apelido, String nome, Foto foto, String senha, Papel papel, Contato contato, String cnpj, List<Demanda> demanda, Armazenamento armazenamento,
 			List<Campanha> campanha, LocalTime horarioInicio, Duration horarioDuracao, List<Doacao> doacoes) {
-		this.usuario = usuario;
+		super(apelido, nome, foto, senha, papel, contato);
 		this.cnpj = cnpj;
 		this.demanda = demanda;
 		this.armazenamento = armazenamento;
