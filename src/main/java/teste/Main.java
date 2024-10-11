345cd66b42b7c3fd67aa23f031f7d4e0a294fc70
HENRIQUE EBERHARDT <henrique.eberhardt@alunos.sc.senac.br>
2024-10-11 16:46:22 -0300

    Atualizada classe main

--- src/main/java/teste/Main.java
+++ src/main/java/teste/Main.java
@@ -21,6 +21,256 @@ public class Main {
 		} finally {
 			System.out.println("Enviado ao banco com Sucesso!");
 		}
+		
+		// Conquista
+				ConquistaDAOImpl conquistaDAO = new ConquistaDAOImpl();
+				Conquista conquista = new Conquista();
+				conquista.setDescricaoConquista("Teste");
+				conquista.setIdConquista(0);
+				conquista.setNomeConquista("Teste");
+		 
+				// Teste de insert de dados
+				try {
+					conquistaDAO.inserirConquista(conquista);
+				} catch (Exception e) {
+					System.out.println("Erro no código!");
+				} finally {
+					System.out.println("Enviado ao banco com Sucesso!");
+				}
+		 
+				// Teste de deletar dados
+				try {
+					conquistaDAO.deletarConquista(conquista);
+				} catch (Exception e) {
+					System.out.println("Erro no código!");
+				} finally {
+					System.out.println("Enviado ao banco com Sucesso!");
+				}
+		 
+				// Teste de atualizar dados
+				try {
+					conquista.setDescricaoConquista("Teste Atualizado");
+					conquistaDAO.atualizarConquista(conquista);
+				} catch (Exception e) {
+					System.out.println("Erro no código!");
+				} finally {
+					System.out.println("Enviado ao banco com Sucesso!");
+				}
+		 
+				// Teste de selecionar dados
+				try {
+					for (Conquista conquistas : conquistaDAO.recuperarConquistas()) {
+						System.out.println("ID: " + conquistas.getIdConquista());
+						System.out.println("Nome: " + conquistas.getNomeConquista());
+						System.out.println("Descrição: " + conquistas.getDescricaoConquista());
+					}
+				} catch (Exception e) {
+					System.out.println("Erro no código!");
+				} finally {
+					System.out.println("Enviado ao banco com Sucesso!");
+				}
+		 
+		 
+		// Papel
+				PapelDAOImpl papelDAO = new PapelDAOImpl();
+				Papel papel = new Papel();
+				papel.setNomePapel("XXXXXX");
+		 
+				// Teste de insert de dados
+				try {
+					papelDAO.inserirPapel(papel);
+				} catch (Exception e) {
+					System.out.println("Erro no código!");
+				} finally {
+					System.out.println("Enviado ao banco com Sucesso!");
+				}
+		 
+				// Teste de deletar dados
+				try {
+					papelDAO.deletarPapel(papel);
+				} catch (Exception e) {
+					System.out.println("Erro no código!");
+				} finally {
+					System.out.println("Enviado ao banco com Sucesso!");
+				}
+		 
+				// Teste de atualizar dados
+				try {
+					papel.setNomePapel("YYYYYY");
+					papelDAO.atualizarPapel(papel);
+				} catch (Exception e) {
+					System.out.println("Erro no código!");
+				} finally {
+					System.out.println("Enviado ao banco com Sucesso!");
+				}
+		 
+				// Teste de selecionar dados
+				try {
+					for (Papel papeis : papelDAO.recuperarPapeis()) {
+						System.out.println("ID: " + papeis.getIdPapel());
+						System.out.println("Nome: " + papeis.getNomePapel());
+						System.out.println("Apelido: " + papeis.getApelido());
+					}
+				} catch (Exception e) {
+					System.out.println("Erro no código!");
+				} finally {
+					System.out.println("Enviado ao banco com Sucesso!");
+				}
+		 
+		// Doacao
+				DoacaoDAOImpl doacaoDAO = new DoacaoDAOImpl();
+				Doador doador = new Doador();
+				Hemocentro hemocentro = new Hemocentro();
+		 
+				Doacao doacao = new Doacao();
+				doacao.setDoador(doador);
+				doacao.setHemocentro(hemocentro);
+				doacao.setDataDoacao(LocalDate.now());
+				doacao.setHoraDoacao(LocalTime.now());
+		 
+				// Teste de insert de dados
+				try {
+					doacaoDAO.inserirDoacao(doacao);
+				} catch (Exception e) {
+					System.out.println("Erro no código!");
+				} finally {
+					System.out.println("Enviado ao banco com Sucesso!");
+				}
+		 
+				// Teste de deletar dados
+				try {
+					doacaoDAO.deletarDoacao(doacao);
+				} catch (Exception e) {
+					System.out.println("Erro no código!");
+				} finally {
+					System.out.println("Enviado ao banco com Sucesso!");
+				}
+		 
+				// Teste de atualizar dados
+				try {
+					doacao.setDataDoacao(LocalDate.of(2023, 10, 10));
+					doacao.setHoraDoacao(LocalTime.of(14, 30));
+					doacaoDAO.atualizarDoacao(doacao);
+				} catch (Exception e) {
+					System.out.println("Erro no código!");
+				} finally {
+					System.out.println("Enviado ao banco com Sucesso!");
+				}
+		 
+				// Teste de selecionar dados
+				try {
+					for (Doacao d : doacaoDAO.recuperarDoacoes()) {
+						System.out.println("ID: " + d.getIdDoacao());
+						System.out.println("Doador: " + d.getDoador().getId());
+		 
+						System.out.println("Hemocentro: " + d.getHemocentro().getId());
+		 
+						System.out.println("Data: " + d.getDataDoacao());
+						System.out.println("Hora: " + d.getHoraDoacao());
+					}
+				} catch (Exception e) {
+					System.out.println("Erro no código!");
+				} finally {
+					System.out.println("Enviado ao banco com Sucesso!");
+				}
+		 
+		 
+		// Endereco
+				EnderecoDAOImpl enderecoDAO = new EnderecoDAOImpl();
+				Endereco endereco = new Endereco();
+				endereco.setEstado("SP");
+				endereco.setCidade("São Paulo");
+				endereco.setBairro("Centro");
+				endereco.setLogradouro("Avenida Paulista");
+				endereco.setNumero(1000);
+				endereco.setCep("12345678");
+		 
+				// Teste de insert de dados
+				try {
+					enderecoDAO.inserirEndereco(endereco);
+				} catch (Exception e) {
+					System.out.println("Erro no código!");
+				} finally {
+					System.out.println("Enviado ao banco com Sucesso!");
+				}
+		 
+				// Teste de deletar dados
+				try {
+					enderecoDAO.deletarEndereco(endereco);
+				} catch (Exception e) {
+					System.out.println("Erro no código!");
+				} finally {
+					System.out.println("Enviado ao banco com Sucesso!");
+				}
+		 
+				// Teste de atualizar dados
+				try {
+					endereco.setLogradouro("Rua Nova");
+					endereco.setNumero(2000);
+					enderecoDAO.atualizarEndereco(endereco);
+				} catch (Exception e) {
+					System.out.println("Erro no código!");
+				} finally {
+					System.out.println("Enviado ao banco com Sucesso!");
+				}
+		 
+		// Endereco
+				EnderecoDAOImpl enderecoDAO = new EnderecoDAOImpl();
+				Endereco endereco = new Endereco();
+				endereco.setEstado("SP");
+				endereco.setCidade("São Paulo");
+				endereco.setBairro("Centro");
+				endereco.setLogradouro("Avenida Paulista");
+				endereco.setNumero(1000);
+				endereco.setCep("12345678");
+		 
+				// Teste de insert de dados
+				try {
+					enderecoDAO.inserirEndereco(endereco);
+				} catch (Exception e) {
+					System.out.println("Erro no código!");
+				} finally {
+					System.out.println("Enviado ao banco com Sucesso!");
+				}
+		 
+				// Teste de deletar dados
+				try {
+					enderecoDAO.deletarEndereco(endereco);
+				} catch (Exception e) {
+					System.out.println("Erro no código!");
+				} finally {
+					System.out.println("Enviado ao banco com Sucesso!");
+				}
+		 
+				// Teste de atualizar dados
+				try {
+					endereco.setLogradouro("Rua Nova");
+					endereco.setNumero(2000);
+					enderecoDAO.atualizarEndereco(endereco);
+				} catch (Exception e) {
+					System.out.println("Erro no código!");
+				} finally {
+					System.out.println("Enviado ao banco com Sucesso!");
+				}
+		 
+				// Teste de selecionar dados
+				try {
+					for (Endereco enderecos : enderecoDAO.recuperarEnderecos()) {
+						System.out.println("ID: " + enderecos.getIdEndereco());
+						System.out.println("Estado: " + enderecos.getEstado());
+						System.out.println("Cidade: " + enderecos.getCidade());
+						System.out.println("Bairro: " + enderecos.getBairro());
+						System.out.println("Logradouro: " + enderecos.getLogradouro());
+						System.out.println("Número: " + enderecos.getNumero());
+						System.out.println("CEP: " + enderecos.getCep());
+					}
+		 
+				} catch (Exception e) {
+					System.out.println("Erro no código!");
+				} finally {
+					System.out.println("Enviado ao banco com Sucesso!");
+				}
+		 
 
 		
 
