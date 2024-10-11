307c2d8a9441b6ee1fa83f6567db95a9796a9af6
HENRIQUE EBERHARDT <henrique.eberhardt@alunos.sc.senac.br>
2024-10-11 14:40:08 -0300

    Corrigido DAO da classe Doacao

--- src/main/java/br/senac/hemolink/modelo/dao/doacao/DoacaoDAOImpl.java
+++ src/main/java/br/senac/hemolink/modelo/dao/doacao/DoacaoDAOImpl.java
@@ -11,100 +11,139 @@ import org.hibernate.Session;
 import br.senac.hemolink.modelo.entidade.doacao.Doacao;
 import br.senac.hemolink.modelo.factory.conexao.ConexaoFactory;
 
-public class DoacaoDAOImpl implements DoacaoDAO{
-	
+public class DoacaoDAOImpl implements DoacaoDAO {
+
 	private ConexaoFactory fabrica;
-	
-	public DoacaoDAOImpl () {
+
+	public DoacaoDAOImpl() {
 		fabrica = new ConexaoFactory();
 	}
 
 	public void inserirDoacao(Doacao doacao) {
-		
+
 		Session sessao = null;
-		
+
 		try {
-			
+
 			sessao = fabrica.getConexao().openSession();
 			sessao.beginTransaction();
-			
+
 			sessao.save(doacao);
-			
+
 			sessao.getTransaction().commit();
-			
-		} catch (Exception  sqlException) {
-			
+
+		} catch (Exception sqlException) {
+
 			sqlException.printStackTrace();
-			
+
 			if (sessao.getTransaction() != null) {
 				sessao.getTransaction().rollback();
 			}
-			
+
 		} finally {
-			
+
 			if (sessao != null) {
 				sessao.close();
 			}
 		}
-		
+
 	}
 
 	public void deletarDoacao(Doacao doacao) {
-		
+
 		Session sessao = null;
-		
+
 		try {
-			
+
 			sessao = fabrica.getConexao().openSession();
 			sessao.beginTransaction();
-			
+
 			sessao.delete(doacao);
-			
+
 			sessao.getTransaction().commit();
-			
+
 		} catch (Exception sqlException) {
-			
+
 			sqlException.printStackTrace();
-			
+
 			if (sessao.getTransaction() != null) {
 				sessao.getTransaction().rollback();
 			}
-			
+
 		} finally {
-			
+
 			if (sessao != null) {
 				sessao.close();
 			}
 		}
-		
+
 	}
 
 	public void atualizarDoacao(Doacao doacao) {
-		
+
 		Session sessao = null;
-		
+
 		try {
 			sessao = fabrica.getConexao().openSession();
 			sessao.beginTransaction();
-			
+
 			sessao.update(doacao);
-			
+
 			sessao.getTransaction().commit();
-			
-		}catch (Exception sqlException) {
-			
+
+		} catch (Exception sqlException) {
+
 			sqlException.printStackTrace();
-			
+
 			if (sessao.getTransaction() != null) {
 				sessao.getTransaction().rollback();
 			}
-			
+
 		} finally {
-			
+
 			if (sessao != null) {
 				sessao.close();
 			}
 		}
+	}
+
+	public List<Doacao> recuperarDoacoes() {
+
+		Session sessao = null;
+		List<Doacao> doacoes = null;
+
+		try {
+
+			sessao = fabrica.getConexao().openSession();
+			sessao.beginTransaction();
+
+			CriteriaBuilder construtor = sessao.getCriteriaBuilder();
+
+			CriteriaQuery<Doacao> criteria = construtor.createQuery(Doacao.class);
+			Root<Doacao> raizDoacao = criteria.from(Doacao.class);
+
+			criteria.select(raizDoacao);
+
+			doacoes = sessao.createQuery(criteria).getResultList();
+
+			sessao.getTransaction().commit();
+
+		} catch (Exception sqlException) {
+
+			sqlException.printStackTrace();
+
+			if (sessao.getTransaction() != null) {
+				sessao.getTransaction().rollback();
+			}
+
+		} finally {
+
+			if (sessao != null) {
+				sessao.close();
+			}
+		}
+
+		return doacoes;
 	}
 
 }
