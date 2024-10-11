d8d2f205dd8182de1e4c02fee058d8bdb7754480
HENRIQUE EBERHARDT <henrique.eberhardt@alunos.sc.senac.br>
2024-10-11 14:40:58 -0300

    Corrigido DAO da classe Doacao

--- src/main/java/br/senac/hemolink/modelo/dao/doacao/DoacaoDAO.java
+++ src/main/java/br/senac/hemolink/modelo/dao/doacao/DoacaoDAO.java
@@ -1,5 +1,7 @@
 package br.senac.hemolink.modelo.dao.doacao;
 
+import java.util.List;
+
 import br.senac.hemolink.modelo.entidade.doacao.Doacao;
 
 public interface DoacaoDAO {
@@ -9,4 +11,6 @@ public interface DoacaoDAO {
 	void deletarDoacao (Doacao doacao);
 	
 	void atualizarDoacao (Doacao doacao);
+	
+	List<Doacao> recuperarDoacoes();
 }
