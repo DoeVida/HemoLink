439158329f5b9623dde4aed09066c9f8fb7b693b
HENRIQUE EBERHARDT <henrique.eberhardt@alunos.sc.senac.br>
2024-10-18 15:42:51 -0300

    Atualizado DAO da classe Doacao

---
+++ src/main/java/br/senac/hemolink/modelo/dao/doacao/DoacaoDAO.java
@@ -0,0 +1,16 @@
+package br.senac.hemolink.modelo.dao.doacao;
+
+import java.util.List;
+
+import br.senac.hemolink.modelo.entidade.doacao.Doacao;
+
+public interface DoacaoDAO {
+		
+	void inserirDoacao (Doacao doacao);
+	
+	void deletarDoacao (Doacao doacao);
+	
+	void atualizarDoacao (Doacao doacao);
+	
+	List<Doacao> recuperarDoacoes();
+}
