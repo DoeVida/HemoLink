ea5793401684ff747c367d1cc4d45c6b299d53b7
paozinho6254 <antoniorochadasilvaa@gmail.com>
2024-10-30 16:48:24 -0300

    Mapeamentos atualizados

--- src/main/java/br/senac/hemolink/main/Teste.java
+++ src/main/java/br/senac/hemolink/main/Teste.java
@@ -1,48 +1,59 @@
 package br.senac.hemolink.main;
- 
+
 import java.time.LocalDate;
- 
+
 import br.senac.hemolink.modelo.dao.foto.FotoDAO;
 import br.senac.hemolink.modelo.dao.foto.FotoDAOImpl;
 import br.senac.hemolink.modelo.dao.usuario.UsuarioDAO;
 import br.senac.hemolink.modelo.dao.usuario.UsuarioDAOImpl;
 import br.senac.hemolink.modelo.entidade.foto.Foto;
+import br.senac.hemolink.modelo.entidade.usuario.Usuario;
 import br.senac.hemolink.modelo.entidade.usuario.doador.Doador;
 import br.senac.hemolink.modelo.enumeracao.TipoSanguineo;
- 
+
 public class Teste {
- 
+
 	public static void main(String[] args) {
+
+		UsuarioDAOImpl usuarioDAO = new UsuarioDAOImpl();
+
+		Usuario usuario = new Doador();
+
+		usuario.setApelido("ddd");
+		usuario.setId(null);
+		// Teste de insert de dados
+		try {
+			usuarioDAO.inserirUsuario(usuario);
+		} catch (Exception e) {
+			System.out.println("Erro no código!");
+		} finally {
+			System.out.println("Acabou");
+		}
+
 		/*
 		 * Foto foto = new Foto(); foto.setConteudo(new byte[2]);
 		 * foto.setExtensao("sim"); foto.setUsuario(null);
 		 * 
 		 * FotoDAO fotoDAO = new FotoDAOImpl(); fotoDAO.inserirFoto(foto);
+		 * 
+		 * UsuarioDAO usuarioDAO = new UsuarioDAOImpl(); Doador usuario = new Doador();
+		 * 
+		 * usuario.setApelido("Marii");
+		 * usuario.setEmail("antoniorochadasilvaa@gmail.com"); usuario.setContato(null);
+		 * usuario.setCpf("09495049478");
+		 * usuario.setDataDeNascimento(LocalDate.parse("1212-12-12"));
+		 * usuario.setFoto(null); usuario.setNome("mariana"); usuario.setSenha("123");
+		 * usuario.setSexo('f'); usuario.setTipoSanguineo(TipoSanguineo.O_NEGATIVO);
+		 * usuario.setPapel(null);
+		 * 
+		 * usuarioDAO.inserirUsuario(usuario);
+		 * 
+		 * // Long id = 1L;
+		 * 
+		 * // usuario.setId(id); // usuarioDAO.atualizarUsuario(usuario);
+		 * 
+		 * // usuarioDAO.deletarUsuario(usuario);
 		 */
-		UsuarioDAO usuarioDAO = new UsuarioDAOImpl();
-		Doador usuario = new Doador();
- 
-		usuario.setApelido("Marii");
-		usuario.setEmail("antoniorochadasilvaa@gmail.com");
-		usuario.setContato(null);
-		usuario.setCpf("09495049478");
-		usuario.setDataDeNascimento(LocalDate.parse("1212-12-12"));
-		usuario.setFoto(null);
-		usuario.setNome("mariana");
-		usuario.setSenha("123");
-		usuario.setSexo('f');
-		usuario.setTipoSanguineo(TipoSanguineo.O_NEGATIVO);
-		usuario.setPapel(null);
- 
-		usuarioDAO.inserirUsuario(usuario);
- 
-		// Long id = 1L;
- 
-		// usuario.setId(id);
-		// usuarioDAO.atualizarUsuario(usuario);
- 
-		// usuarioDAO.deletarUsuario(usuario);
- 
 	}
- 
+
 }
