e276f50e21dcf08ef3ea9a23646c0a650fb0c05b
paozinho6254 <antoniorochadasilvaa@gmail.com>
2024-10-30 16:17:12 -0300

    Adicionado atributo email a usuario, adicionado recuperarUsuario pelo email em impl e dao, e linkados ao usuario central

--- src/main/java/br/senac/hemolink/main/Teste.java
+++ src/main/java/br/senac/hemolink/main/Teste.java
@@ -1,7 +1,7 @@
 package br.senac.hemolink.main;
-
+ 
 import java.time.LocalDate;
-
+ 
 import br.senac.hemolink.modelo.dao.foto.FotoDAO;
 import br.senac.hemolink.modelo.dao.foto.FotoDAOImpl;
 import br.senac.hemolink.modelo.dao.usuario.UsuarioDAO;
@@ -9,9 +9,9 @@ import br.senac.hemolink.modelo.dao.usuario.UsuarioDAOImpl;
 import br.senac.hemolink.modelo.entidade.foto.Foto;
 import br.senac.hemolink.modelo.entidade.usuario.doador.Doador;
 import br.senac.hemolink.modelo.enumeracao.TipoSanguineo;
-
+ 
 public class Teste {
-
+ 
 	public static void main(String[] args) {
 		/*
 		 * Foto foto = new Foto(); foto.setConteudo(new byte[2]);
@@ -21,8 +21,9 @@ public class Teste {
 		 */
 		UsuarioDAO usuarioDAO = new UsuarioDAOImpl();
 		Doador usuario = new Doador();
-
+ 
 		usuario.setApelido("Marii");
+		usuario.setEmail("antoniorochadasilvaa@gmail.com");
 		usuario.setContato(null);
 		usuario.setCpf("09495049478");
 		usuario.setDataDeNascimento(LocalDate.parse("1212-12-12"));
@@ -32,16 +33,16 @@ public class Teste {
 		usuario.setSexo('f');
 		usuario.setTipoSanguineo(TipoSanguineo.O_NEGATIVO);
 		usuario.setPapel(null);
-
+ 
 		usuarioDAO.inserirUsuario(usuario);
-
+ 
 		// Long id = 1L;
-
+ 
 		// usuario.setId(id);
 		// usuarioDAO.atualizarUsuario(usuario);
-
+ 
 		// usuarioDAO.deletarUsuario(usuario);
-
+ 
 	}
-
+ 
 }
--- src/main/java/br/senac/hemolink/modelo/entidade/usuario/hemocentro/Hemocentro.java
+++ src/main/java/br/senac/hemolink/modelo/entidade/usuario/hemocentro/Hemocentro.java
@@ -1,10 +1,10 @@
 package br.senac.hemolink.modelo.entidade.usuario.hemocentro;
-
+ 
 import java.io.Serializable;
 import java.time.Duration;
 import java.time.LocalTime;
 import java.util.List;
-
+ 
 import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
@@ -13,8 +13,9 @@ import javax.persistence.JoinColumn;
 import javax.persistence.MapsId;
 import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
+import javax.persistence.PrimaryKeyJoinColumn;
 import javax.persistence.Table;
-
+ 
 import br.senac.hemolink.modelo.entidade.armazenamento.Armazenamento;
 import br.senac.hemolink.modelo.entidade.campanha.Campanha;
 import br.senac.hemolink.modelo.entidade.contato.Contato;
@@ -24,49 +25,49 @@ import br.senac.hemolink.modelo.entidade.endereco.Endereco;
 import br.senac.hemolink.modelo.entidade.foto.Foto;
 import br.senac.hemolink.modelo.entidade.papel.Papel;
 import br.senac.hemolink.modelo.entidade.usuario.Usuario;
-
+ 
 @Entity
 @Table(name = "hemocentro")
+@PrimaryKeyJoinColumn(name = "id_usuario")
 public class Hemocentro extends Usuario implements Serializable {
-
+ 
 	private static final long serialVersionUID = 5082517849085199550L;
-
+ 
 	@Column(name = "cnpj_hemocentro", length = 14, nullable = false, unique = true)
 	private String cnpj;
-
+ 
 	@OneToMany(fetch = FetchType.LAZY, mappedBy = "hemocentro", cascade = CascadeType.REMOVE)
 	private List<Demanda> demanda;
-
+ 
 	@OneToOne(fetch = FetchType.LAZY)
 	@MapsId
 	@JoinColumn(name = "id_armazenamento")
 	private Armazenamento armazenamento;
-
+ 
 	@OneToMany(fetch = FetchType.LAZY, cascade = CascadeType.REMOVE)
 	private List<Campanha> campanhas;
-
+ 
 	@Column(name = "horario_abertura_hemocentro", nullable = false)
 	private LocalTime horarioInicio;
-
+ 
 	@Column(name = "horario_fechamento_hemocentro", nullable = false)
 	private Duration horarioDuracao;
-
+ 
 	@OneToOne(fetch = FetchType.LAZY)
 	@MapsId
 	@JoinColumn(name = "id_endereco")
 	private Endereco endereco;
-
+ 
 	@OneToMany(fetch = FetchType.LAZY, mappedBy = "hemocentro", cascade = CascadeType.DETACH)
 	private List<Doacao> doacoes;
-
+ 
 	public Hemocentro() {
 	}
-
+ 
 	public Hemocentro(Long id, String apelido, String nome, Foto foto, String senha, Papel papel, Contato contato, String cnpj,
 			List<Demanda> demanda, Armazenamento armazenamento, List<Campanha> campanha, LocalTime horarioInicio,
 			Duration horarioDuracao, List<Doacao> doacoes) {
-
-		super(id, apelido, nome, foto, senha, papel, contato);
+		super(id, apelido, nome, cnpj, foto, senha, papel, contato);
 		this.cnpj = cnpj;
 		this.demanda = demanda;
 		this.armazenamento = armazenamento;
@@ -75,67 +76,67 @@ public class Hemocentro extends Usuario implements Serializable {
 		this.horarioDuracao = horarioDuracao;
 		this.doacoes = doacoes;
 	}
-
+ 
 	public String getCnpj() {
 		return cnpj;
 	}
-
+ 
 	public void setCnpj(String cnpj) {
 		this.cnpj = cnpj;
 	}
-
+ 
 	public List<Demanda> getDemanda() {
 		return demanda;
 	}
-
+ 
 	public void setDemanda(List<Demanda> demanda) {
 		this.demanda = demanda;
 	}
-
+ 
 	public Armazenamento getArmazenamento() {
 		return armazenamento;
 	}
-
+ 
 	public void setArmazenamento(Armazenamento armazenamento) {
 		this.armazenamento = armazenamento;
 	}
-
+ 
 	public List<Campanha> getCampanhas() {
 		return campanhas;
 	}
-
+ 
 	public void setCampanhas(List<Campanha> campanhas) {
 		this.campanhas = campanhas;
 	}
-
+ 
 	public LocalTime getHorarioInicio() {
 		return horarioInicio;
 	}
-
+ 
 	public void setHorarioInicio(LocalTime horarioInicio) {
 		this.horarioInicio = horarioInicio;
 	}
-
+ 
 	public Duration getHorarioDuracao() {
 		return horarioDuracao;
 	}
-
+ 
 	public void setHorarioDuracao(Duration horarioDuracao) {
 		this.horarioDuracao = horarioDuracao;
 	}
-
+ 
 	public Endereco getEndereco() {
 		return endereco;
 	}
-
+ 
 	public void setEndereco(Endereco endereco) {
 		this.endereco = endereco;
 	}
-
+ 
 	public List<Doacao> getDoacoes() {
 		return doacoes;
 	}
-
+ 
 	public void setDoacoes(List<Doacao> doacoes) {
 		this.doacoes = doacoes;
 	}
