273499c4b39870708acd2e8438b06157d7c1a842
Charlesna2guerra <antoniorochadasilvaa@gmail.com>
2024-11-12 17:10:15 -0300

    Atualizado Servlet de Doacao

--- src/main/java/br/senac/hemolink/controle/servlet/DoadorServlet.java
+++ src/main/java/br/senac/hemolink/controle/servlet/DoadorServlet.java
@@ -76,7 +76,6 @@ public class DoadorServlet extends HttpServlet {
     private void listarDoadors(HttpServletRequest request, HttpServletResponse response) throws SQLException, IOException, ServletException {
         List<Doador> doador = daoDoador.recuperarDoadores();
         request.setAttribute("Doador", doador);
-
         RequestDispatcher dispatcher = request.getRequestDispatcher("listar-Doador.jsp");
         dispatcher.forward(request, response);
     }
